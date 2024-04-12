84ba65ec3f8f80e93b9f4f6d3e58325d4ef45275
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2024-04-12 19:19:36 +0530

    updatedd

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
+// import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
@@ -28,7 +28,15 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
+const connectToMongoDB= async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_DB_URI)
+        console.log("connected to uri")
+    }
+    catch(error){
+        console.log("Error connecting to mongodb",error.message);
+    }
+}
 server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
